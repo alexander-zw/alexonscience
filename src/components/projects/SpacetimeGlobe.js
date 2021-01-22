7507f91c46b0719df1e75763cd428e11c4ed7916
Alexander Wu <alexanderwu68@yahoo.com>
2021-01-21 22:35:26 -0800

    Create selector to add new colored stars

--- src/components/projects/SpacetimeGlobe.js
+++ src/components/projects/SpacetimeGlobe.js
@@ -21,53 +21,7 @@ import InputAdornment from "@material-ui/core/InputAdornment";
 
 import "../../styles/index.css";
 import "../../styles/projects/SpacetimeGlobe.css";
-
-class SpacetimeEvent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { t0: props.t0, x0: props.x0, t: props.t0, x: props.x0 };
-    }
-
-    updateReferenceFrame = (gamma, v) => {
-        const { t0, x0 } = this.state;
-        this.setState({
-            t: gamma * (t0 - v * x0),
-            x: gamma * (x0 - v * t0),
-        });
-    };
-
-    onDragEnd = (e) => {
-        this.setState({
-            t0: e.target.y(),
-            x0: e.target.x(),
-        });
-    };
-
-    render() {
-        const { fill, draggable } = this.props;
-        const { x, t } = this.state;
-        return (
-            <Star
-                x={x}
-                y={t}
-                numPoints={5}
-                innerRadius={0.13}
-                outerRadius={0.3}
-                rotation={36}
-                fill={fill ? fill : "green"}
-                draggable={draggable}
-                onDragEnd={this.onDragEnd}
-            />
-        );
-    }
-}
-
-SpacetimeEvent.propTypes = {
-    t0: PropTypes.number.isRequired,
-    x0: PropTypes.number.isRequired,
-    fill: PropTypes.string,
-    draggable: PropTypes.bool,
-};
+import hello_there from "../../images/hello_there.png";
 
 function ReferenceFrameInput(props) {
     const marks = [
@@ -133,29 +87,129 @@ ReferenceFrameInput.propTypes = {
     onChange: PropTypes.func,
 };
 
-const left = -4;
-const right = 4;
-const top = 5;
-const bottom = -2;
+const imageData = [
+    {
+        color: "red",
+    },
+    {
+        color: "green",
+    },
+    {
+        color: "blue",
+    },
+    {
+        color: "gold",
+    },
+    {
+        color: "purple",
+    },
+    {
+        color: "brown",
+    },
+    {
+        color: "cyan",
+    },
+    {
+        color: "grey",
+    },
+];
+
+function EventSelector(props) {
+    return (
+        <div className="event-selector-div">
+            Add an event:
+            <div className="event-selector-outer">
+                <div className="event-selector">
+                    {imageData.map((image, i) => (
+                        <img
+                            className="event-image"
+                            style={{ backgroundColor: image.color }}
+                            src={hello_there}
+                            alt={image.color}
+                            onClick={() => props.onSelect(image)}
+                            key={i}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+EventSelector.propTypes = {
+    onSelect: PropTypes.func,
+};
+
+class SpacetimeEvent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { t0: props.t0, x0: props.x0, t: props.t0, x: props.x0 };
+    }
+
+    updateReferenceFrame = (gamma, v) => {
+        const { t0, x0 } = this.state;
+        this.setState({
+            t: gamma * (t0 - v * x0),
+            x: gamma * (x0 - v * t0),
+        });
+    };
+
+    onDragEnd = (e) => {
+        this.setState({
+            t0: e.target.y(),
+            x0: e.target.x(),
+        });
+    };
+
+    render() {
+        const { fill, draggable } = this.props;
+        const { x, t } = this.state;
+        return (
+            <Star
+                x={x}
+                y={t}
+                numPoints={5}
+                innerRadius={0.13}
+                outerRadius={0.3}
+                rotation={36}
+                fill={fill ? fill : "green"}
+                draggable={draggable}
+                onDragEnd={this.onDragEnd}
+            />
+        );
+    }
+}
+
+SpacetimeEvent.propTypes = {
+    t0: PropTypes.number.isRequired,
+    x0: PropTypes.number.isRequired,
+    fill: PropTypes.string,
+    draggable: PropTypes.bool,
+};
+
+const LEFT = -4;
+const RIGHT = 4;
+const TOP = 5;
+const BOTTOM = -2;
 
 function Grid() {
     const arrowLen = 0.3;
 
     const lines = [];
     // Horizontal line.
-    for (let i = bottom; i <= top; i++) {
+    for (let i = BOTTOM; i <= TOP; i++) {
         if (i == 0) {
-            lines.push([left - arrowLen, i, right + arrowLen, i]);
+            lines.push([LEFT - arrowLen, i, RIGHT + arrowLen, i]);
         } else {
-            lines.push([left, i, right, i]);
+            lines.push([LEFT, i, RIGHT, i]);
         }
     }
     // Vertical line.
-    for (let i = left; i <= right; i++) {
+    for (let i = LEFT; i <= RIGHT; i++) {
         if (i == 0) {
-            lines.push([i, bottom - arrowLen, i, top + arrowLen]);
+            lines.push([i, BOTTOM - arrowLen, i, TOP + arrowLen]);
         } else {
-            lines.push([i, bottom, i, top]);
+            lines.push([i, BOTTOM, i, TOP]);
         }
     }
 
@@ -186,12 +240,12 @@ function hyperbola(start, end, shift, step, isHorizontal) {
         const y = Math.sign(shift) * Math.sqrt(x * x + shift * shift);
         // Ignore if out of bounds.
         if (isHorizontal) {
-            if (bottom <= y && y <= top) {
+            if (BOTTOM <= y && y <= TOP) {
                 points.push(x);
                 points.push(y);
             }
         } else {
-            if (left <= y && y <= right) {
+            if (LEFT <= y && y <= RIGHT) {
                 points.push(y);
                 points.push(x);
             }
@@ -206,22 +260,22 @@ function EventTrajectories() {
 
     const lines = [];
     // Horizontal hyperbolas: ignore top, bottom, and zero lines.
-    for (let i = bottom + 1; i <= top - 1; i++) {
+    for (let i = BOTTOM + 1; i <= TOP - 1; i++) {
         if (i != 0) {
-            lines.push(hyperbola(left, right, i, renderStep, true));
+            lines.push(hyperbola(LEFT, RIGHT, i, renderStep, true));
         }
     }
     // Vertical hyperbolas: ignore left, right, and zero lines.
-    for (let i = left + 1; i <= right - 1; i++) {
+    for (let i = LEFT + 1; i <= RIGHT - 1; i++) {
         if (i != 0) {
-            lines.push(hyperbola(bottom, top, i, renderStep, false));
+            lines.push(hyperbola(BOTTOM, TOP, i, renderStep, false));
         }
     }
     // Diagonals.
-    const lowerLeft = Math.max(left, bottom);
-    const lowerRight = Math.min(right, -bottom);
-    const upperLeft = Math.min(-left, top);
-    const upperRight = Math.min(right, top);
+    const lowerLeft = Math.max(LEFT, BOTTOM);
+    const lowerRight = Math.min(RIGHT, -BOTTOM);
+    const upperLeft = Math.min(-LEFT, TOP);
+    const upperRight = Math.min(RIGHT, TOP);
     lines.push([lowerLeft, lowerLeft, upperRight, upperRight]);
     lines.push([-upperLeft, upperLeft, lowerRight, -lowerRight]);
 
@@ -232,7 +286,7 @@ function EventTrajectories() {
 
 function Labels(props) {
     // These numbers were through trial and error.
-    const spaceLabelX = right - 0.73;
+    const spaceLabelX = RIGHT - 0.73;
     const spaceLabelY = 0.5;
     return [
         // Rect to block part of the grid.
@@ -257,7 +311,7 @@ function Labels(props) {
         />,
         <Text
             x={0.2}
-            y={top + 0.4}
+            y={TOP + 0.4}
             scaleX={props.scale}
             scaleY={-props.scale}
             fontSize={30}
@@ -273,6 +327,40 @@ Labels.propTypes = {
     scale: PropTypes.number.isRequired,
 };
 
+const eventData = [
+    { t: -1, x: 0, fill: "red" },
+    { t: 0, x: 0, fill: "red" },
+    { t: 1, x: 0, fill: "red" },
+    { t: 2, x: 0, fill: "red" },
+    { t: 3, x: 0, fill: "red" },
+    { t: 4, x: 0, fill: "red" },
+    { t: -1, x: -0.5, fill: "green" },
+    { t: 0, x: 0, fill: "green" },
+    { t: 1, x: 0.5, fill: "green" },
+    { t: 2, x: 1, fill: "green" },
+    { t: 3, x: 1.5, fill: "green" },
+    { t: 4, x: 2, fill: "green" },
+    { t: 0, x: -3, fill: "blue" },
+    { t: 0, x: -2, fill: "blue" },
+    { t: 0, x: -1, fill: "blue" },
+    { t: 0, x: 0, fill: "blue" },
+    { t: 0, x: 1, fill: "blue" },
+    { t: 0, x: 2, fill: "blue" },
+    { t: 0, x: 3, fill: "blue" },
+    { t: -1, x: -1, fill: "gold" },
+    { t: 0, x: 0, fill: "gold" },
+    { t: 1, x: 1, fill: "gold" },
+    { t: 2, x: 2, fill: "gold" },
+    { t: 3, x: 3, fill: "gold" },
+    { t: 4, x: 4, fill: "gold" },
+    { t: -1, x: 2, fill: "purple" },
+    { t: 0, x: 2, fill: "purple" },
+    { t: 1, x: 2, fill: "purple" },
+    { t: 2, x: 2, fill: "purple" },
+    { t: 3, x: 2, fill: "purple" },
+    { t: 4, x: 2, fill: "purple" },
+];
+
 function SpacetimeGlobe() {
     // Most of these values were obtained through trial and error.
     const scale = 80;
@@ -280,40 +368,6 @@ function SpacetimeGlobe() {
     const canvasHeight = 625;
     const canvasOffsetX = (canvasWidth * 0.5) / scale;
     const canvasOffsetY = (canvasHeight * 0.69) / scale;
-
-    const eventData = [
-        { t: -1, x: 0, fill: "red" },
-        { t: 0, x: 0, fill: "red" },
-        { t: 1, x: 0, fill: "red" },
-        { t: 2, x: 0, fill: "red" },
-        { t: 3, x: 0, fill: "red" },
-        { t: 4, x: 0, fill: "red" },
-        { t: -1, x: -0.5, fill: "green" },
-        { t: 0, x: 0, fill: "green" },
-        { t: 1, x: 0.5, fill: "green" },
-        { t: 2, x: 1, fill: "green" },
-        { t: 3, x: 1.5, fill: "green" },
-        { t: 4, x: 2, fill: "green" },
-        { t: 0, x: -3, fill: "blue" },
-        { t: 0, x: -2, fill: "blue" },
-        { t: 0, x: -1, fill: "blue" },
-        { t: 0, x: 0, fill: "blue" },
-        { t: 0, x: 1, fill: "blue" },
-        { t: 0, x: 2, fill: "blue" },
-        { t: 0, x: 3, fill: "blue" },
-        { t: -1, x: -1, fill: "gold" },
-        { t: 0, x: 0, fill: "gold" },
-        { t: 1, x: 1, fill: "gold" },
-        { t: 2, x: 2, fill: "gold" },
-        { t: 3, x: 3, fill: "gold" },
-        { t: 4, x: 4, fill: "gold" },
-        { t: -1, x: 2, fill: "purple" },
-        { t: 0, x: 2, fill: "purple" },
-        { t: 1, x: 2, fill: "purple" },
-        { t: 2, x: 2, fill: "purple" },
-        { t: 3, x: 2, fill: "purple" },
-        { t: 4, x: 2, fill: "purple" },
-    ];
 
     const [draggable, setDraggable] = useState(true);
 
@@ -345,6 +399,14 @@ function SpacetimeGlobe() {
         setDraggable(v == 0); // Can only drag at default reference frame.
     }
 
+    // A state variable used to force re-render.
+    const [updateVar, setUpdateVar] = useState(0);
+
+    function onEventSelect(color) {
+        eventData.push({ t: 0, x: LEFT, fill: color.color });
+        setUpdateVar(updateVar + 1);
+    }
+
     return (
         <div className="outer-container top-margin bottom-margin">
             <Helmet>
@@ -358,6 +420,7 @@ function SpacetimeGlobe() {
 
             <div className="controls text-div">
                 <ReferenceFrameInput onChange={updateReferenceFrame} />
+                <EventSelector onSelect={onEventSelect} />
             </div>
 
             <div className="text-div">
